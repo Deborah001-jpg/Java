3d6de8d9ad150f780b6cd83f59f02f79038f7bbd
Deborah001-jpg <148598992+Deborah001-jpg@users.noreply.github.com>
2023-11-14 17:19:17 -0300

    aula06

--- Aula06/src/aula06/Ativ4Set.java
+++ Aula06/src/aula06/Ativ4Set.java
@@ -1,9 +1,36 @@
 package aula06;
 
-public class Ativ4Set {
+import java.util.HashSet;
+import java.util.Scanner;
 
+public class Ativ4Set {
+	
+	static Scanner leia = new Scanner(System.in);
+	
+	// Criar Set de Wrapper Integer e inicializar com 10 valores inteiros
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		HashSet<Integer> numerosSet = new HashSet<>();
+		
+		    numerosSet.add(2);
+	        numerosSet.add(5);
+	        numerosSet.add(1);
+	        numerosSet.add(3);
+	        numerosSet.add(4);
+	        numerosSet.add(9);
+	        numerosSet.add(7);
+	        numerosSet.add(8);
+	        numerosSet.add(10);
+	        numerosSet.add(6);
+		
+	     // Solicitar ao usuário que digite um número inteiro
+	        
+	        
+	        System.out.print("Digite um número: ");
+	        int numeroDigitado = leia.nextInt();
+		
+	        
 
 	}
 
